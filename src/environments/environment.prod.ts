4813b19f3d32da9ca20b47df10fe2647a693e006
Hoa Nguyen <hoanguyendn61@gmail.com>
2022-12-28 11:26:38 +0700

    fix: changed apiUrl environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiURL: 'https://wanderlust.onrender.com/api',
+  apiURL: "https://wanderlust-api.tk/api",
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  apiURL: 'https://wanderlust.onrender.com/api',
+  apiURL: "https://wanderlust-api.tk/api",
   // apiURL: 'http://localhost:3000/api',
 };
 
